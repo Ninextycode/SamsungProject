fa8e28b3584bdfedd0869d90f4e5f0b44e3e4abd
Ninextycode <ninexty@gmail.com>
2016-05-19 00:09:01 +0300

    Time
    Time reset to normal from fast

--- app/src/main/java/com/max/myapplication/Constants.java
+++ app/src/main/java/com/max/myapplication/Constants.java
@@ -5,7 +5,7 @@ package com.max.myapplication;
  */
 public class Constants {
     public static int mode = 0;
-    public static  int[] timeLimitSeconds = {3*6, 4*6, 5*6, 6*6};
+    public static  int[] timeLimitSeconds = {3*60, 4*60, 5*60, 6*60};
     public static  int[] figureH = {4, 5, 6, 7};
     public static  int[] figureW = {3, 4, 5, 6};
     public static  int[] figureN = {3, 4, 5, 6};
